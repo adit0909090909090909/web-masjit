0822f0f0396cfbc84647560d3c2956387544454b
adit0909090909090909 <madityailham35@gmail.com>
2023-09-29 13:02:26 +0700

    figma to html

--- js/script.js
+++ js/script.js
@@ -1,5 +1,64 @@
 $(document).ready(function(){
+$(".show-popup-infak").click(function(){
+  $(".wrapper-popup").addClass("show");
+});
+
+$(".close,.backdron").click(function(){
+  $(".wrapper-popup").removeClass("show");
+});
+
+
   $('.hamburgel').click(function(){
-      $("top-menu").removeClass('d-none');
-})
-})
+   $(".top-menu").css('right',"0");
+});
+
+$('.close').click(function () {
+  $(".top-menu").css('right',"-100vw");
+});
+
+
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  // autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  // },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+var swiper = new Swiper(".swiper-kegiatan", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+     navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  
+  
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+  },
+});
+});  
